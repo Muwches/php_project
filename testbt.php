207f7f5582b1df40fb65ed4fe3c583fafbe9b88f
Hannington Muwanga <muwches@aol.com>
2018-10-24 18:13:29 -0500

    trial submit

---
+++ testbt.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Checking Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<h2>Hello world!</h2>
+</body>
+</html>
